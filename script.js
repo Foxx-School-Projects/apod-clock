6285f8ec2dc6a8b2fc437e973a1cfe423a1a10d0
Foxx Greeley <gree0626@algonquinlive.com>
2021-12-10 16:29:28 -0500

    pushed greetings

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
 
-// ==== NASA APOD ASSIGNMENT ====
-
-// API KEY: sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz
+// ==== NASA APOD ASSIGNMENT | API KEY: sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz
 
 let date = new Date();
-
 let imageDate = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
 
-console.log('Date is', imageDate)
+console.log('Image Date is', imageDate)
 
 fetch('https://api.nasa.gov/planetary/apod?api_key=sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz&date=' + imageDate + '&thumbs=True')
-  .then(function (response){ //JS promise structure is returned and caputed in response parameter
-    return response.json() //Return data in JSON format when available
+  .then(function (response){ //JS promise structure and response
+    return response.json() //Return data
 })
 .then(function (imageData){ //JSON data captured in this parameter
-  // console.log(imageData) //display incoming data for debug purposes
+  
+  // console.log(imageData) //display image data for debug purposes
 
-  if (imageData.media_type === 'video') { //If the photo is a video, use placeholder
+  if (imageData.media_type === 'video') { 
+    
+    //If the photo is a video, use placeholder
     document.querySelector('p').textContent = 'APOD is a video'
-  } else { //otherwise set src of image to photo url 
 
-    console.log('image url:', imageData.hdurl)
+  } else { 
+
+    // set src of image to photo url 
+    console.log('Image Source:', imageData.hdurl)
 
     document.getElementById('bg-img').style.backgroundImage = 'url(' + imageData.hdurl + ')';
   }
 
 })
+
+
+
+
+// ==== Live Clock ====
+
+console.log('Loaded time is:', date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) )
+
+const $timer = document.getElementById('time');
+const $date = document.getElementById('date');
+
+
+// Function Ticking Starts
+let clock = setInterval(function clockStart () {
+let currentDate = new Date();
+
+$timer.innerHTML = (`<h1 class='intergers'>${currentDate.toLocaleTimeString()}</h1>`);
+$date.innerHTML = (`<h1>${currentDate.toDateString()}</h1>`);
+
+
+
+// Update Greeting Depending on Time
+
+let motd = document.getElementById('motd');
+
+// If past 12am and before 12pm
+if ((date.getHours() > 0) && (date.getHours() < 12) ) {
+  motd.innerHTML = '<h1>Good Morning!</h1>';
+} 
+
+// If past 12pm and before 6pm
+else if ((date.getHours() > 12) && (date.getHours() < 18) ) {
+  motd.innerHTML = '<h1>Good Afternoon!</h1>';
+} 
+
+// If past 6pm
+else if (date.getHours() > 18) {
+  motd.innerHTML = '<h1>Good Evening!</h1>';
+} 
+
+
+
+
+}, 0, 1000);
