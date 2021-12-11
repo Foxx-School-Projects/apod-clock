6b952fda4ca79cbc76da3af6569dc1dcb89a847d
Foxx Greeley <gree0626@algonquinlive.com>
2021-12-10 22:27:46 -0500

    cleaned code and finsihed first setting

--- script.js
+++ script.js
@@ -1,70 +1,137 @@
 
-// ==== NASA APOD ASSIGNMENT | API KEY: sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz
-
+// ==== NASA APOD | API KEY: sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz
 let date = new Date();
 let imageDate = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
 
-console.log('Image Date is', imageDate)
-
+// Fetch API
 fetch('https://api.nasa.gov/planetary/apod?api_key=sdcCicWmBL9lc51EcwZNp64dDHpMJpnhb5WO5Xgz&date=' + imageDate + '&thumbs=True')
-  .then(function (response){ //JS promise structure and response
-    return response.json() //Return data
+  .then(function (response){  //JS promise structure and response
+    return response.json()    //Return data
 })
-.then(function (imageData){ //JSON data captured in this parameter
+.then(function (imageData){   //JSON data captured in this parameter
   
-  // console.log(imageData) //display image data for debug purposes
+  // console.log(imageData)   //display image data for debug purposes
 
   if (imageData.media_type === 'video') { 
     
     //If the photo is a video, use placeholder
     document.querySelector('p').textContent = 'APOD is a video'
 
+    // Else Continue
   } else { 
 
-    // set src of image to photo url 
-    console.log('Image Source:', imageData.hdurl)
-
-    console.log(imageData)
-
+    // Set Div Background Image
     document.getElementById('bg-img').style.backgroundImage = 'url(' + imageData.hdurl + ')';
   
+    // Contents of More Info Block
     let $imginfo = document.getElementById('extra-data');
     $imginfo.innerHTML = 
     (`<div class="container">
         <div class="row">
-          <div class="datablock col"><h2>Source</h2>` + imageData.hdurl + `</div>
-          <div class="datablock col"><h2>Image Title</h2>` + imageData.title + `</div>
+          <div class="datablock col"><h2>Image Title</h2><p>` + imageData.title + ` by ` + imageData.copyright + `</p></div>
+
           <div class="w-100"></div>
-          <div class="datablock col"><h2>Image Title</h2>` + imageData.title + `</div>
-          <div class="datablock col"><h2>Image Author</h2>` + imageData.copyright + `</div>
+          <div class="datablock col"><h2>Image Description</h2><p>` + imageData.explanation + `</p></div>
+
         </div>`);
-
   }
-  
-
 })
+
+
+
+
+
+// ==== Settings | Show Seconds ====
+
+// Select Input
+let $checkbox = document.querySelector("input[name=seconds]");
+
+// ===== IF PREVIOUS DATA EXIST =====
+if (localStorage.getItem('checkboxstatus') !== null) {
+
+  // Log Status
+  console.log('== Previous Data Found ==');
+
+  // Destring Data
+  let tempObject = JSON.parse(localStorage.getItem('checkboxstatus'));
+
+  // Log Data Found
+  console.log('Past Setting is', tempObject.Checkbox);
+
+  // Checkbox Value = old object value
+  $checkbox.value = tempObject.Checkbox;
+
+} 
+  
+// If Checkbox Value = On |  Checkbox Checked
+if ( ($checkbox.value) === 'on' ) {
+  $checkbox.checked = true;
+} 
+
+// Else If Checkbox Value = Off |  Checkbox Unchecked
+else if ( ($checkbox.value) === 'off' ){
+  $checkbox.checked = false;
+}
+
+
+// Event Listener |  When Checkbox Toggled
+$checkbox.addEventListener('change', function() {
+
+  // If Checked | Value = on
+  if ($checkbox.checked) {
+    $checkbox.value = 'on'
+  } 
+  
+  // If Not | Value = off
+  else {
+    $checkbox.value = 'off'
+  }
+
+  // ====== Store Setting | Local Storage =======
+
+  // Local Storage | Store Data
+  const objectDataToStore = {
+    Checkbox: $checkbox.value,
+  };
+
+  // Store Info Under 'checkbox', Convert to string
+  localStorage.setItem('checkboxstatus', JSON.stringify(objectDataToStore));
+
+});
+
+
+
 
 
 
 
 // ==== Live Clock ====
 
-console.log('Loaded time is:', date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) )
-
+// Data
 const $timer = document.getElementById('time');
 const $date = document.getElementById('date');
 
 
-// Function Ticking Starts
+// Start Clock
 let clock = setInterval(function clockStart () {
+
+// currentDate = Current Date/Time
 let currentDate = new Date();
 
-$timer.innerHTML = (`<h1 class='intergers'>${currentDate.toLocaleTimeString()}</h1>`);
+// Set's Default Time Format
 $date.innerHTML = (`<h1>${currentDate.toDateString()}</h1>`);
 
+// If setting on, show seconds. Otherwise, don't.
+if ($checkbox.value === 'on'){
+  $timer.innerHTML = (`<h1 class='intergers'>${currentDate.toLocaleTimeString()}</h1>`);
+} else if ($checkbox.value === 'off'){
+  $timer.innerHTML = (`<h1 class='intergers'>${date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</h1>`);
+}
 
 
-// Update Greeting Depending on Time
+
+
+// = Greeting Depending on Time =
 
 let motd = document.getElementById('motd');
 
@@ -83,16 +150,21 @@ else if ((date.getHours() >= 18) && (date.getHours() <= 23) ) {
   motd.innerHTML = '<h1>Good Evening!</h1>';
 } 
 
-
+// Tick Rate
 }, 0, 1000);
+
+
+
+
 
 
 // ==== Settings Button Integration ====
 
-// Click Event Listener
+// Data
 const $settingsButton = document.getElementById('settings');
 const $togcontent = document.getElementById('settingblock');
 
+// Click Event Listener
 $settingsButton.addEventListener('click', function (e) {
 
   // Target Button
@@ -101,6 +173,7 @@ $settingsButton.addEventListener('click', function (e) {
   // If Present
   if ($selectedInput){
 
+    // Toggle Display Classes
     $togcontent.classList.toggle("dnone");
     $togcontent.classList.toggle("dblock");
 
@@ -109,12 +182,12 @@ $settingsButton.addEventListener('click', function (e) {
 
 
 
-// ==== Info Button Integration ====
-
-// Click Event Listener
+// ==== More Info Button Integration ====
+// Data
 const $infoButton = document.getElementById('infotog');
 const $infoCont = document.getElementById('infoblock');
 
+// Click Event Listener
 $infoButton.addEventListener('click', function (e) {
 
   // Target Button
@@ -123,8 +196,11 @@ $infoButton.addEventListener('click', function (e) {
   // If Present
   if ($selectedInput){
 
+    // Toggle Display Classes
     $infoCont.classList.toggle("dnone");
     $infoCont.classList.toggle("dblock");
 
   }
 });
+
+
